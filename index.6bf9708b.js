c4ae474c90498c46f5d75ebe9e391c193286f7e9
Kseniia Karpenko <ksushakarpenko635@gmail.com>
2024-09-25 17:56:07 +0200

    make build

---
+++ index.6bf9708b.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");e.style.position="relative",t.style.position="absolute";var l=e.clientHeight/2-t.clientHeight/2,i=e.clientWidth/2-t.clientWidth/2;t.style.top=l+"px",t.style.left=i+"px";
+//# sourceMappingURL=index.6bf9708b.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const image = document.querySelector(".spider");
+wall.style.position = "relative";
+image.style.position = "absolute";
+const centerH = wall.clientHeight / 2 - image.clientHeight / 2;
+const centerW = wall.clientWidth / 2 - image.clientWidth / 2;
+image.style.top = centerH + "px";
+image.style.left = centerW + "px";
+
+//# sourceMappingURL=index.f75de5e1.js.map
